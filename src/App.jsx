8ef28e70b92fb395657702a539a0fdf58efa63db
The GreatPratyush <pratushgupta27199@gmail.com>
2026-04-25 11:38:50 +0530

    adding animation css

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ const App = () => {
   const [solvedGrid, setSolvedGrid] = useState([]);
   const [chancesLeft, setChancesLeft] = useState(5);
   const [wrongCells, setWrongCells] = useState({});
-  const [gameOver, setGameOver] = useState(false); // ✅ ADDED
+  const [gameOver, setGameOver] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   async function handleStartGame() {
     try {
-      setGameOver(false); 
+      setGameOver(false);
+      setImageLoaded(false);
 
       const response = await fetch(
         "https://suduko-generator-andsolver.onrender.com/generate"
@@ -68,7 +70,7 @@ const App = () => {
         const next = prev - 1;
 
         if (next === 0) {
-          setGameOver(true); 
+          setGameOver(true);
           setGrid(solvedGrid);
         }
 
@@ -114,6 +116,20 @@ const App = () => {
     return classes.join(" ");
   }
 
+if (!imageLoaded && showGame) {
+  return (
+    <div className="loader">
+      Loading...
+      <img
+        src="/appBackground.png"
+        alt="preload"
+        style={{ display: "none" }}
+        onLoad={() => setImageLoaded(true)}
+        onError={() => setImageLoaded(true)}
+      />
+    </div>
+  );
+}
   return (
     <div>
       {!showGame ? (
@@ -121,11 +137,9 @@ const App = () => {
       ) : (
         <div className="backgroundApp">
 
-         
           <div style={{ fontSize: "24px", marginBottom: "10px" }} className="hearts">
             {"❤️".repeat(chancesLeft)}
           </div>
-
 
           {gameOver && (
             <div className="game-over-overlay">
